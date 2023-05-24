a27e244594a5e0f248d9c86f6b873e65de73f7e1
cindylu165 <cindylu242@gmail.com>
2023-05-25 01:38:33 +0800

    update

--- polar_chart_app.py
+++ polar_chart_app.py
@@ -62,7 +62,7 @@ else:
     end_date = pd.to_datetime('2022-12', format='%Y-%m')
     filtered_df = _df[(_df['monitormonth'] >= start_date) & (_df['monitormonth'] <= end_date)]
     # 篩選地區
-    loc = filtered_df[filtered_df['sitename']==selected_location]
+    loc = filtered_df[filtered_df['sitename']==location]
     loc = loc.replace('x', 0)
     loc = loc.fillna(0)
     loc = pd.pivot_table(loc, values='concentration', index=loc['monitormonth'], columns='itemname')
