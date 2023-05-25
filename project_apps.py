6a2869af470afc250d4ecc468d545feb02717141
cindylu165 <cindylu242@gmail.com>
2023-05-25 15:21:23 +0800

    update

--- project_apps.py
+++ project_apps.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
+# 使用 st.markdown 設定標籤的字體大小
+st.markdown("<style> .stTab > button { font-size: 18px; } </style>", unsafe_allow_html=True)
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
