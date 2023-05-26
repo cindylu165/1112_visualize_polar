cdc252df2a79f98a184f2e7eb4311de12fcf7233
cindylu165 <cindylu242@gmail.com>
2023-05-26 09:59:45 +0800

    update

---
+++ pages/Q2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# 設定網頁config
+st.set_page_config(page_title="Air Quality Monitoring", page_icon="☁️", layout="wide")
+# 設定網頁標題
+st.title("Question 2")
--- project_apps.py
+++ project_apps.py
@@ -1,29 +1,12 @@
 import streamlit as st
 
+# 設定網頁config
+st.set_page_config(page_title="Air Quality Monitoring", page_icon="☁️", layout="wide")
 # 設定網頁標題
 st.title("Air Quality Monitoring")
 
 # 使用 st.markdown 設定標籤的字體大小
 st.markdown("<style> .stTab > button { font-size: 24px; } </style>", unsafe_allow_html=True)
 
-
-with st.container():
-    tabs = st.tabs(["chat1", "chat2", "chat3"])
-    
-    if tabs[0]:
-        with st.sidebar:
-            st.write("Sidebar for Tab 1")
-        
-        st.write("Content of Tab 1")
-        
-    elif tabs[1]:
-        with st.sidebar:
-            st.write("Sidebar for Tab 2")
-        
-        st.write("Content of Tab 2")
-        
-    elif tabs[2]:
-        with st.sidebar:
-            st.write("Sidebar for Tab 3")
-        
-        st.write("Content of Tab 3")
+st.sidebar.markdown('Please choose an Question')
+st.sidebar.title('parameter setting')
