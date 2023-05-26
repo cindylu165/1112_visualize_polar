df06d4c57e45f842fc4fa9309af6a084eecb9652
cindylu165 <cindylu242@gmail.com>
2023-05-26 11:22:59 +0800

    update

--- pages/Q2.py
+++ pages/Q2.py
@@ -3,7 +3,6 @@ import streamlit as st
 # 設定網頁config
 st.set_page_config(page_title="Air Quality Monitoring", page_icon="⛅", layout="wide")
 # 設定網頁標題
-st.title("Question 2")
 
 # reference : https://docs.streamlit.io/library/api-reference/charts/st.plotly_chart
 
