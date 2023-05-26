2340e89f0487082f890b3b81bd69793210258842
cindylu165 <cindylu242@gmail.com>
2023-05-26 11:01:44 +0800

    a

--- project_apps.py
+++ project_apps.py
@@ -6,10 +6,6 @@ from pyvis.network import Network
 import plotly.graph_objects as go
 import numpy as np
 
-_df = pd.read_csv("./data.csv")
-_df = _df.replace('x', np.nan)
-_df = _df.dropna()
-
 # 設定網頁config
 st.set_page_config(page_title="Air Quality Monitoring", page_icon="☁️", layout="wide")
 # 設定網頁標題
@@ -20,18 +16,21 @@ st.markdown("<style> .stTab > button { font-size: 24px; } </style>", unsafe_allo
 
 st.sidebar.success('Please choose an Question above')
 
-# st.markdown(
-#     """ ### Introduction 
-#     - Currently, air pollution has become an urgent environmental issue that needs to be addressed.The haze caused by ozone, sulfur dioxide, and other PM2.5 pollutants can lead to other environmental problems. Acid rain is one of the major issues caused by industrial areas, and almost the entire Taiwan is now affected by acid rain. Therefore, we have decided to analyze the current trends of air pollution and acid rain, hoping to encourage everyone to take concrete actions to protect the environment.  
-#     """, unsafe_allow_html=True)
-_df = _df[['sitename','itemname','itemengname','itemunit','monitormonth','concentration']]
-st.dataframe(_df, use_container_width = True)
+st.markdown(
+    """ ### Introduction 
+    - Currently, air pollution has become an urgent environmental issue that needs to be addressed.The haze caused by ozone, sulfur dioxide, and other PM2.5 pollutants can lead to other environmental problems. Acid rain is one of the major issues caused by industrial areas, and almost the entire Taiwan is now affected by acid rain. Therefore, we have decided to analyze the current trends of air pollution and acid rain, hoping to encourage everyone to take concrete actions to protect the environment.  
+    """, unsafe_allow_html=True)
 
 
+
+_df = pd.read_csv("./data.csv")
+_df = _df.replace('x', np.nan)
+_df = _df.dropna()
 
 st.markdown(
     f""" ### Dataset
     - The dataset is from [Air Quality Monitoring Network](https://data.epa.gov.tw/dataset/detail/AQX_P_08 )
     - The dataset contains {_df.shape[1]} columns and {_df.shape[0]} rows
-    """)
-
+    """, unsafe_allow_html=True)
+_df = _df[['sitename','itemname','itemengname','itemunit','monitormonth','concentration']]
+st.dataframe(_df, use_container_width = True)
