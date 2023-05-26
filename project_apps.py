2c74cfe0186ae70ea6f4eab93ee07405cfd34231
cindylu165 <cindylu242@gmail.com>
2023-05-26 10:47:20 +0800

    update

--- project_apps.py
+++ project_apps.py
@@ -17,7 +17,9 @@ st.markdown("<style> .stTab > button { font-size: 24px; } </style>", unsafe_allo
 st.sidebar.success('Please choose an Question above')
 
 st.markdown(
-    """ ### Introduction  """)
+    """ ### Introduction 
+    - Currently, air pollution has become an urgent environmental issue that needs to be addressed.The haze caused by ozone, sulfur dioxide, and other PM2.5 pollutants can lead to other environmental problems. Acid rain is one of the major issues caused by industrial areas, and almost the entire Taiwan is now affected by acid rain. Therefore, we have decided to analyze the current trends of air pollution and acid rain, hoping to encourage everyone to take concrete actions to protect the environment.  
+    """)
 
 
 
